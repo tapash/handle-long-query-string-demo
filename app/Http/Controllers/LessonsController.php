7abe72165cb16906fc49a00c828df30792025b48
Tapas Datta <tapash.me@gmail.com>
2017-08-04 17:41:06 +0600

    route update

--- app/Http/Controllers/LessonsController.php
+++ app/Http/Controllers/LessonsController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Lesson;
 
 class LessonsController extends Controller
 {
-    //
+    public function show()
+    {
+    	$lessions = Lesson::all();
+    	return $lessions;
+    }
 }
